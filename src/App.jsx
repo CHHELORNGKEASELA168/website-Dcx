705d35030a1b7d07e34f5cc9bb089d27bbef7050
Chhel orngkeasela <chhelangkeasela@gmail.com>
2025-11-26 09:15:24 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Footer from './Component/Footer';
-import Header from './Component/Header';
-import Rout from './Component/Rout';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Rout from './components/Rout';
 
 function App() {
   return (
